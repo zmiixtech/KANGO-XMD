5d3bdeae4e7d16891511c18eae950677fc1e23b7
OfficialKango <kangonathaniel@gmail.com>
2025-04-14 13:46:00 +0000

    Add files via upload

---
+++ Media/tikquote.js
@@ -0,0 +1,7 @@
+module.exports = [
+  "https://vt.tiktok.com/ZSr9ba6qp/",
+  "https://vt.tiktok.com/ZSr9b6RJp/",
+  "https://vt.tiktok.com/ZSr9bBKnP/",
+  "https://vt.tiktok.com/ZSr9bF7QC/",
+  "https://vt.tiktok.com/ZSr9bUtyg/"
+];
