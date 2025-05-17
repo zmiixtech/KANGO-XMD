6883cc3246d61d63caec09338d61228298835171
OfficialKango <kangonathaniel@gmail.com>
2025-05-17 04:02:27 +0000

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,81 @@
+//-------------------[ BOT SETTINGS ]------------------// 
+
+// @project_name : KANGO-XMD 
+// @author : Hector 
+// @telegram : http://t.me/official_kango
+// @github : OfficialKango
+// @whatsapp : +233509977126
+
+//----------------------[ KANGO-XMD ]----------------------//
+
+const fs = require('fs')
+const { color } = require('./lib/color')
+if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
+
+
+//--------------------[ SESSION ID ]----------------------//
+
+global.SESSION_ID = process.env.SESSION_ID || '' 
+//Enter your KANGO-XMD session id here; must start with KANGO~
+
+//--------------------[ BOT NAME ]----------------------//
+
+global.botname = process.env.BOT_NAME || 'KANGO-XMD' 
+
+//-----------------[ OWNER NUMBER ]------------------//
+
+global.ownernumber = process.env.OWNER_NUMBER || '233509977126' 
+
+//--------------------[ SUDO ]--------------------------//
+
+global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['233509977126', '233577860202'];
+// Type additional allowed users here
+//NB: They'll be able to use every functions of the bot without restrictions.
+
+//-----------------[ OWNER NAME ]------------------//
+
+global.ownername = process.env.OWNER_NAME || 'Hector Manuel' 
+
+//------------[ STICKER PACKNAME ]-----------------//
+
+global.packname = process.env.STICKER_PACK_NAME || "KANGO-XMD" 
+
+//--------------[ STICKER AUTHOR NAME ]------------//
+
+global.author = process.env.STICKER_AUTHOR_NAME || "Hector" 
+
+//----------------[ GITHUB DATABASE ]-----------------//
+
+global.dbToken = process.env.GITHUB_TOKEN || "";
+
+
+//-----------------[ CONTEXT LINK ]--------------------//
+
+global.plink = process.env.PLINK || "https://youtube.com/@official_manuel"
+
+//------------------[ WATERMARK ]--------------------//
+
+global.wm = process.env.GL_WM || "> ©KANGO-XMD"
+
+//---------------------[ REPLIES ]-----------------------//
+
+global.mess = { 
+  done: '*Done*', 
+  success: '©kango-xmd', 
+  owner: `*You don't have permission to use this command!*`, 
+  group: '*This feature becomes available when you use it in a group!*', 
+  admin: '*You’ll unlock this feature with me as an admin!*', 
+  notadmin: '*This feature will work once you become an admin. A way of ensuring order!*' 
+}
+
+//--------------------[ WATCHER ]-----------------------//
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(color(`Updated '${__filename}'`, 'red'))
+  delete require.cache[file]
+  require(file)
+})
+
+//----------------------[ KANGO-XMD ]----------------------//
